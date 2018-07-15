b97a5d0d1fea1f0518d487d3c6768bdbe3158335
Emad Rawashdeh <erawashdeh@medrah.com>
2018-07-15 22:10:21 +0300

    03 - Deleting/Checking elements as done on ToDo list

--- todo-spec.js
+++ todo-spec.js
@@ -4,7 +4,7 @@ describe('angularjs homepage todo list', function() {
 	
 	//Make sure that the most important elements are present on the page
 	element(by.model('formData.text')).isDisplayed();
-	element(by.buttonText('Add')).isDisplayed();
+	Test that removing one or more items work.isDisplayed();
 	
 	//Test that adding one or more to-do-list-items work
 	element(by.model('formData.text')).sendKeys('Item#1');
@@ -22,7 +22,11 @@ describe('angularjs homepage todo list', function() {
 	var todoList = element.all(by.repeater('todo in todos'));
     expect(todoList.count()).toEqual(3);
 	
-	
+	//Test that removing one or more items work
+	var todoList = element.all(by.repeater('todo in todoList.todos'));
+	todoList.get(0).element(by.css('input')).click();
+	todoList.get(1).element(by.css('input')).click();
+	todoList.get(1).element(by.css('input')).click();
 	
   });
 });
