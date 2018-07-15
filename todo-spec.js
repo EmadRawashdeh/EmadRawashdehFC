7e462348907c0140ed969ebf3753c0b11331e5ef
Emad Rawashdeh <erawashdeh@medrah.com>
2018-07-15 21:38:51 +0300

    02 - Adding New elements to ToDo list and check them

--- todo-spec.js
+++ todo-spec.js
@@ -6,6 +6,23 @@ describe('angularjs homepage todo list', function() {
 	element(by.model('formData.text')).isDisplayed();
 	element(by.buttonText('Add')).isDisplayed();
 	
+	//Test that adding one or more to-do-list-items work
+	element(by.model('formData.text')).sendKeys('Item#1');
+	element(by.buttonText('Add')).click();
+	var todoList = element.all(by.repeater('todo in todos'));
+    expect(todoList.count()).toEqual(1);
+	
+	element(by.model('formData.text')).sendKeys('Item#2');
+	element(by.buttonText('Add')).click();
+	var todoList = element.all(by.repeater('todo in todos'));
+    expect(todoList.count()).toEqual(2);
+	
+	element(by.model('formData.text')).sendKeys('Item#3');
+	element(by.buttonText('Add')).click();
+	var todoList = element.all(by.repeater('todo in todos'));
+    expect(todoList.count()).toEqual(3);
+	
+	
 	
   });
 });
